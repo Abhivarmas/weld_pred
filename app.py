66f13886ac62b820f4e8b5cc45dcb0adab44ef28
Abhivarmas <abhishekvarmas98@yahoo.com>
2022-10-13 23:18:21 +0530

    done

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import session
+import pickle
+
+import os
+from werkzeug.utils import secure_filename
+
+import numpy as np
+
+
+
+ 
+app = Flask(__name__)
+
+@app.route('/') 
+def home():
+    return render_template('first.html')
+ 
+@app.route('/text',methods=['POST'])
+def text():
+    
+    int_features = [float(x)for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    model=pickle.load(open('saved_metal_model','rb'))
+    ans=model.predict(final_features)
+    #return render_template("first.html",prediction=ans)
+    for i in range(0,len(ans)):
+        if ans[i]==0:
+            return render_template("first.html",prediction="No Defect")
+        if ans[i]==1:
+            return render_template("first.html",prediction="Tungsten inclusion")
+        if ans[i]==2:
+            return render_template("first.html",prediction="Porosity")
+        
+
+        
+            
+    
+
+    
+    
+    
+    
+    
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
+
